7ba3bbff952ac98c2504ff8d6401d6f8e3638017
Ke Sun <Ke.Sun@data61.csiro.au>
2019-06-16 16:34:35 +1000

    synchronize with upstream

--- dwmstatus.c
+++ dwmstatus.c
@@ -1,4 +1,3 @@
-#define _DEFAULT_SOURCE
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +23,7 @@ const char* sound_card = "hw:0";
 const char* battery = "/sys/class/power_supply/BAT0";
 
 void settz(const char *tzname) {
-	setenv( "TZ", tzname, 1 );
+  setenv( "TZ", tzname, 1 );
 }
 
 void setstatus( const char *str, Display *dpy ) {
@@ -51,81 +50,49 @@ const char * readfile( const char *base, const char *file ) {
 }
 
 const char * getbattery( const char *base ) {
-    static char ret[512];
+    static char ret[128];
     const char *co;
-    char *stat;
     char status;
-    int descap;
-    int remcap;
-    double remaining;
-    double using;
-    double voltage;
-    double current;
-
-    descap = -1;
-    remcap = -1;
-    using  = -1;
-    remaining = -1;
-    stat = "Not Charging";
+    int descap = -1;
+    int remcap = -1;
 
     co = readfile( base, "status" );
-    if ( co == NULL ) { co = "Not Charging"; }
-    sscanf( co, "%s", &status );
-
-    co = readfile( base, "charge_full_design" );
-    if (co == NULL) {
-        co = readfile( base, "energy_full_design" );
-        if ( co == NULL ) return "";
+    if( co == NULL ) {
+        status = '?';
+    } else if( strncmp(co, "Discharging", 11) == 0 ) {
+        status = '-';
+    } else if( strncmp(co, "Charging", 8) == 0 ) {
+        status = '+';
+    } else if( strncmp(co, "Full", 4 ) == 0 ) {
+        return "Charged";
+    } else {
+        status = '?';
     }
-    sscanf( co, "%d", &descap );
+
+    co = readfile( base, "charge_full" );
+    if( co == NULL ) co = readfile( base, "energy_full" );
+    if( co == NULL ) {
+        status = '?';
+    } else {
+        sscanf( co, "%d", &descap );
+    }
 
     co = readfile( base, "charge_now" );
-    if (co == NULL) {
-        co = readfile( base, "energy_now" );
-        if (co == NULL) return "";
-    }
-    sscanf( co, "%d", &remcap );
-
-    co = readfile( base, "power_now" ); /* µWattage being used */
-    if (co == NULL) {
-        co = readfile( base, "voltage_now" );
-        sscanf( co, "%lf", &voltage );
-
-        co = readfile( base, "current_now" );
-        sscanf( co, "%lf", &current );
-
-        remcap  = (voltage / 1000.0) * ((double)remcap / 1000.0);
-        descap  = (voltage / 1000.0) * ((double)descap / 1000.0);
-        using   = (voltage / 1000.0) * ((double)current / 1000.0);
-
+    if( co == NULL ) co = readfile( base, "energy_now" );
+    if( co == NULL ) {
+        status = '?';
     } else {
-        sscanf( co, "%lf", &using );
+        sscanf( co, "%d", &remcap );
     }
 
-    if (remcap < 0 || descap < 0) return "invalid";
-
-    if (status == 'D') {
-        remaining = (double)remcap / using;
-        stat = "B";
-
-    } else if (status == 'C') {
-        remaining = ((double)descap - (double)remcap) /using;
-        stat = "C";
-
-    } else {
-        remaining = 0;
-        stat = "F";
+    if( remcap < 0 || descap < 0 ) {
+        status = '?';
     }
 
-    if( stat[0] == 'F' )
-        snprintf( ret, sizeof(ret), "Charged" );
-    else {
-        if ( remaining < 0 )
-            snprintf( ret, sizeof(ret), "%s: Calculating...", stat );
-        else
-            snprintf( ret, sizeof(ret), "%s: %.0lf%%(%.01fH)", stat,
-                (((double)remcap / (double)descap) * 100), remaining );
-    }
+    if( status == '?' ) return "battery fault";
+
+    snprintf( ret, sizeof(ret), "%c%.0lf%%",
+              status, ((double)remcap / (double)descap * 100) );
 
     return ret;
 }
@@ -181,22 +148,19 @@ int getvol( long* outvol ) {
 }
 
 const char * mktimes( const char *fmt, const char *tzname ) {
-	static char buf[128];
-	time_t tim;
-	struct tm *timtm;
+    static char buf[128];
+    time_t tim;
+    struct tm *timtm;
 
-	settz( tzname );
-	tim = time(NULL);
-	timtm = localtime(&tim);
-	if (timtm == NULL) {
-		perror("localtime");
-		exit(1);
-	}
+    settz( tzname );
+    tim = time(NULL);
+    timtm = localtime(&tim);
+    if( timtm == NULL ) { perror("localtime"); exit(1); }
 
-	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
-		fprintf(stderr, "strftime == 0\n");
-		exit(1);
-	}
+    if( !strftime(buf, sizeof(buf)-1, fmt, timtm) ) {
+        fprintf(stderr, "strftime == 0\n");
+        exit(1);
+    }
 
     return buf;
 }
@@ -211,8 +175,8 @@ char * getram(void) {
 }
 
 int main(void) {
-	char status[512];
-	const char *_time = NULL;
+    char status[512];
+    const char *_time = NULL;
     const char *_batt = NULL;
     const char *_ram = NULL;
     long _vol;
@@ -224,12 +188,12 @@ int main(void) {
         _batt = getbattery( battery );
         _ram  = getram();
         getvol( &_vol );
-        _time = mktimes( "%a %d %b %H:%M", tz_current );
+        _time = mktimes( "%a-%d-%b %H:%M", tz_current );
 
         snprintf( status, 512, "%s %s ♪%ld %s", _batt, _ram, _vol, _time );
         setstatus( status, dpy );
-	}
+  }
 
     XCloseDisplay( dpy );
-	return 0;
+    return 0;
 }
